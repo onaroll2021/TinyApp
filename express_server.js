123aed3b5ffc4737765d7f7ad314b932e8a4809b
onaroll2021 <lukeli.onaroll@gmail.com>
2022-10-04 03:18:17 +0000

    URL post

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+function generateRandomString() {};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +17,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
